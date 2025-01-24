081fb91e40e11dc28178a725558862791978ac5f
steven.alenga <steven.alenga@gmail.com>
2025-01-24 19:33:20 +0300

    "layout update"

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,15 +9,7 @@ import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] })
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
